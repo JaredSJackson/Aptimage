47c5c5952ec882c25f8b4f455e94134b62428f13
emGHA <42726830+emGHA@users.noreply.github.com>
2019-11-12 21:51:05 -0500

    AnalyticsUser
    All the editing to do the "hover to get tech name" and the "make the links to employee pages" is in "AnalyticsUser.js"

--- Backup/analysis page source files/AnalyticsUser.js
+++ Backup/analysis page source files/AnalyticsUser.js
@@ -1,14 +1,17 @@
-import * as React from 'react';
+import * as React from "react";
 import {
   VictoryBar,
   VictoryChart,
   VictoryPie,
   VictoryAnimation,
-  VictoryLabel
-} from 'victory';
-import { DatePicker } from 'antd';
-import axios from 'axios';
-import 'antd/dist/antd.css';
+  VictoryLabel,
+  VictoryTooltip,
+  VictoryVoronoiContainer
+} from "victory";
+import { DatePicker, Popover, Button } from "antd";
+import axios from "axios";
+import "antd/dist/antd.css";
+import { Link, NavLink } from "react-router-dom";
 // import { throwStatement } from '@babel/types';
 const { RangePicker } = DatePicker;
 function onChange(date, dateString) {
@@ -39,7 +42,7 @@ class AnalyticsUser extends React.Component {
   componentDidMount = () => {
     // test call to the list of tech ids
     axios
-      .post('https://aptimage.net/API/GetTechID.aspx', {})
+      .post("https://aptimage.net/API/GetTechID.aspx", {})
       .then(function(response) {
         // turns the response into string then to an object so the data can be shown
         employeeArray = JSON.parse(
@@ -55,12 +58,12 @@ class AnalyticsUser extends React.Component {
 
     // gets the data from the database upon page load for the progress circle
     axios
-      .post('https://aptimage.net/API/GetTechRatingTotal.aspx', {})
+      .post("https://aptimage.net/API/GetTechRatingTotal.aspx", {})
       .then(function(response) {
         // turns the response into string
         var strThis = JSON.stringify(response.data.ratingsFound[0]).toString();
         // gets the total number from the API
-        progressCirclePercent = parseInt(strThis.replace(/[^0-9.]/g, ''));
+        progressCirclePercent = parseInt(strThis.replace(/[^0-9.]/g, ""));
       })
       .catch(function(error) {
         console.log(error);
@@ -83,7 +86,15 @@ class AnalyticsUser extends React.Component {
   }
 
   getData(percent) {
-    return [{ x: 1, y: percent }, { x: 2, y: 100 - percent }];
+    return [
+      { x: 1, y: percent },
+      { x: 2, y: 100 - percent }
+    ];
+  }
+
+  clickLink() {
+    // <link>click here</link>;
+    console.log("was able to at least clikc");
   }
 
   render() {
@@ -100,11 +111,17 @@ class AnalyticsUser extends React.Component {
             <h1>Total Progress Made Using AptImage According to Users</h1>
 
             {/* Progress circle */}
-            <svg viewBox="0 0 500 500" width="50%" height="50%">
+            <svg
+              viewBox="0 0 500 500"
+              width="50%"
+              height="50%"
+              // responsive={false} // does nothing to the circle
+            >
               <VictoryPie
+                // responsive={false} // does nothing to the circle
                 standalone={false} //this shows the progress line
                 animate={{ duration: 1000 }}
-                width={400}
+                // width={400}
                 height={400}
                 data={this.state.data}
                 innerRadius={120}
@@ -113,8 +130,8 @@ class AnalyticsUser extends React.Component {
                 style={{
                   data: {
                     fill: ({ datum }) => {
-                      const color = datum.y > 30 ? 'green' : 'red';
-                      return datum.x === 1 ? color : 'transparent';
+                      const color = datum.y > 30 ? "green" : "red";
+                      return datum.x === 1 ? color : "transparent";
                     }
                   }
                 }}
@@ -140,30 +157,78 @@ class AnalyticsUser extends React.Component {
           <div>
             <h1>Progress by Employee</h1>
             <VictoryChart
-              height={400}
-              width={400}
+              height={800}
+              width={800}
+              fontSize={50}
               domainPadding={{ x: 50, y: [0, 20] }}
+              //label for the tech names
+              // labelComponent={<VictoryTooltip />}
+              containerComponent={
+                <VictoryVoronoiContainer
+                  responsive={false} // makes the graph a set size
+                  mouseFollowTooltips
+                  voronoiDimension="x" // allows for hovering over the x values
+                  labels={({ datum }) => `tech name: ${datum.x}`} // displays the labels
+                  // labels={({ datum }) => `tech id: ${datum.x}`}
+                />
+              }
             >
-              {/* {(this.barPercent = this.firstCall())} */}
-              {/* {console.log(barPercent)} */}
               <VictoryBar
                 horizontal
+                events={[
+                  {
+                    target: "data",
+                    eventHandlers: {
+                      // onClick: () => <link>click here</link>
+                      onClick: () => {
+                        // <Link>click here</Link>;
+                        return [
+                          {
+                            target: "labels",
+                            mutation: props => {
+                              return props.text === "clicked"
+                                ? null
+                                : { text: "clicked" };
+                            }
+                          }
+                        ];
+                      }
+                    }
+                  }
+                ]}
                 style={{
                   data: {
                     fill: ({ datum }) =>
                       datum.y <= 50
-                        ? '#c91ac4' // purple
+                        ? "#c91ac4" // purple
                         : datum.y >= 51 && datum.y <= 69
-                        ? '#f0cf16' // yellow
+                        ? "#f0cf16" // yellow
                         : datum.y >= 70 && datum.y <= 89
-                        ? '#8fcbff' // light blue
+                        ? "#8fcbff" // light blue
                         : datum.y >= 90 && datum.y <= 100
-                        ? 'green'
-                        : 'black'
-                  }
+                        ? "green"
+                        : "black"
+                  },
+                  labels: { fontSize: 25 },
+                  fontSize: 25
                 }}
                 // data needs to be taken from api
-                data={employeeArray}
+                data={[
+                  { x: "joe", y: 30 },
+                  { x: "seven", y: 55 },
+                  { x: "zell", y: 100 },
+                  { x: "ren", y: 70 },
+                  { x: "paq", y: 30 },
+                  { x: "netta", y: 55 },
+                  { x: "tero", y: 100 },
+                  { x: "ras", y: 70 },
+                  { x: "ker", y: 30 },
+                  { x: "owm", y: 55 },
+                  { x: "yev", y: 100 },
+                  { x: "bekkin", y: 70 }
+                ]}
+                // labels={({ datum }) => <Popover>`${datum.x}`</Popover>}
+                // labels={({ datum }) => `${datum.x}%`}
                 labels={({ datum }) => `${datum.y}%`}
               />
             </VictoryChart>
@@ -176,18 +241,31 @@ class AnalyticsUser extends React.Component {
 
 // example data to show how the x values should be displayed
 // [
-//   { x: 'joe', y: 30 },
-//   { x: 'seven', y: 55 },
-//   { x: 'zell', y: 100 },
-//   { x: 'ren', y: 70 },
-//   { x: 'paq', y: 30 },
-//   { x: 'netta', y: 55 },
-//   { x: 'tero', y: 100 },
-//   { x: 'ras', y: 70 },
-//   { x: 'ker', y: 30 },
-//   { x: 'owm', y: 55 },
-//   { x: 'yev', y: 100 },
-//   { x: 'bekkin', y: 70 }
+// { x: 'joe', y: 30 },
+// { x: 'seven', y: 55 },
+// { x: 'zell', y: 100 },
+// { x: 'ren', y: 70 },
+// { x: 'paq', y: 30 },
+// { x: 'netta', y: 55 },
+// { x: 'tero', y: 100 },
+// { x: 'ras', y: 70 },
+// { x: 'ker', y: 30 },
+// { x: 'owm', y: 55 },
+// { x: 'yev', y: 100 },
+// { x: 'bekkin', y: 70 }
 // ]
+
+// { x: 'joe', y: 30, label: 'tech 1' },
+// { x: 'seven', y: 55, label: 'tech 2' },
+// { x: 'zell', y: 100, label: 'tech 3' },
+// { x: 'ren', y: 70, label: 'tech 4' },
+// { x: 'paq', y: 30, label: 'tech 5' },
+// { x: 'netta', y: 55, label: 'tech 6' },
+// { x: 'tero', y: 100, label: 'tech 7' },
+// { x: 'ras', y: 70, label: 'tech 8' },
+// { x: 'ker', y: 30, label: 'tech 9' },
+// { x: 'owm', y: 55, label: 'tech 10' },
+// { x: 'yev', y: 100, label: 'tech 11' },
+// { x: 'bekkin', y: 70, label: 'tech 12' }
 
 export default AnalyticsUser;
